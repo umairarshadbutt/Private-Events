1ff255863e9ac9dbb752abfb5cca2e96ef85f788
Umair Arshad <Umair6622@gmail.com>
2020-09-30 03:42:11 +0500

    set root to sessions#welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   resources :users, :only => [:new, :create, :show]
+  root 'sessions#welcome'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-  get '/welcome' => 'sessions#welcome'
+  # get '/welcome' => 'sessions#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
