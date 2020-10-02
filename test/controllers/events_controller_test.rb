ea8bb79d9414eb7e805cedcd7dadddde82006c25
Umair Arshad <Umair6622@gmail.com>
2020-10-02 14:37:55 +0500

    Test cases added

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get index' do
+    get events_index_url
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get events_new_url
+    assert_response :success
+  end
+
+  test 'should get create' do
+    get events_create_url
+    assert_response :success
+  end
+
+  test 'should get show' do
+    get events_show_url
+    assert_response :success
+  end
 end
